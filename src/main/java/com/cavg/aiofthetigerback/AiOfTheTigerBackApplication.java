28aa5a00533f924236ba5f84e0e19ec442d41a67
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-05-18 18:47:48 -0500

    Initial commit

---
+++ src/main/java/com/cavg/aiofthetigerback/AiOfTheTigerBackApplication.java
@@ -0,0 +1,13 @@
+package com.cavg.aiofthetigerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiOfTheTigerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiOfTheTigerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cavg/aiofthetigerback/AiOfTheTigerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavg.aiofthetigerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiOfTheTigerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
